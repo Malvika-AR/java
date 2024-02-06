a03999fa59bb9148bec726a2d238574b64948a66
Malvika <malvikarasotra@gmail.com>
2024-02-06 17:56:37 +0530

    this is commit

---
+++ Demo_test.java
@@ -0,0 +1,11 @@
+package New_test;
+
+public class Demo_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("25");
+	}
+
+}
